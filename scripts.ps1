f4df6b3c06af5e189bd805b41cc4f03f33b5fc47
Kenzuya <pbgarena0838@gmail.com>
2024-01-12 18:39:15 +0700

    Update scripts.ps1

--- scripts.ps1
+++ scripts.ps1
@@ -1,16 +1,29 @@
-$UltraviewerUrl = "https://download2269.mediafire.com/sgpd3edhc1pg99SpcwjVEsM4NQTZKrOcPkA3fi4PvpmPLP22n7w1L-csQ0MHxZAPPG3T7GvZoU4nTEIc-RyzXU1yAFU2Z5uVtdl9Rf_GvV6jlJl3pLh1NB-5fjWnVigcAUP4Jk-1vKekLEU8cDbpJl7JFylFZuHAdVQGgZa-IQzbUP4/jwcgpuoclv11f00/UltraViewer_setup_6.6_en.exe"
-$DRMUrl = "https://download942.mediafire.com/thz0mt23x8rgst0G3d3canEaBVQNd3fLDWPz4inJrX8iNn-XDRvqrrXK-bRX4h7VGYheArJNv3-PNv6HS-pC7hFVl6Wraq_zeDFH9OGhCRgUTiEumXiuAd2K1cmdPr4J1H6dxyCuSNtNlfDhag00WnNEMxTuTcP2n7HO5Paw_NCs4hc/etdm165mcqqbun6/video-drm-tools-setup-2.0.0.exe"
+$UltraviewerUrl = "https://dl2.ultraviewer.net/UltraViewer_setup_6.6_en.exe"
+$DRMUrl = "https://download1594.mediafire.com/rtw9no3bfqzgO_f67z_RLkWh29icp75AhCMs88GsxaFfaubng6nLln2y24L1ZPVlBJitzu6qnNwJLiqzvQIWaPYV1W5hyGwH_reKG_ellhl1xrMnAPvR52R5ClNOBTXEBXIC0glGrW7qIbbRo2x8nvAk98rRFcxUazKhVqiSKvgynXlD/b9e1tnwfpk7q825/video-drm-tools-setup-2.0.0.exe"
+$GoogleDriveUrl = "https://dl.google.com/drive-file-stream/GoogleDriveFSSetup.exe"
 $ProgressPreference = 'SilentlyContinue'
 Write-Host "Downloading UltraViewer..."
 Invoke-WebRequest -Uri $UltraviewerUrl -OutFile "$env:TEMP\UltraViewer.exe"
 # curl --progress-bar $UltraviewerUrl -o "$env:TEMP\UltraViewer.exe"
-Write-Host "Downloading DRM Tools..."
-Invoke-WebRequest -Uri $DRMUrl -OutFile "$env:TEMP\drm-tools.exe"
+# Write-Host "Downloading DRM Tools..."
+# Invoke-WebRequest -Uri $DRMUrl -OutFile "$env:TEMP\drm-tools.exe"
 # curl --progress-bar $DRMUrl -o "$env:TEMP\drm-tools.exe
 
 Write-Host "Installing UltraViewer..."
-& "$env:TEMP\UltraViewer.exe" /VERYSILENT /NORESTART
+cmd /c "$env:TEMP\UltraViewer.exe" /DIR="C:\UltraViewer" /VERYSILENT /NORESTART
+# Write-Host "Installing DRM Tools..."
+# & "$env:TEMP\drm-tools.exe" /SILENT
+
+& "C:\UltraViewer\UltraViewer_Desktop.exe"
+
+Write-Host "Downloading Google Drive..."
+Invoke-WebRequest -Uri $GoogleDriveUrl -OutFile "$env:TEMP\GoogleDriveFSSetup.exe"
+
+Write-Host "Installing Google Drive..."
+cmd /c "$env:TEMP\GoogleDriveFSSetup.exe" --silent
+
+Write-Host "Downloading DRM Tools..."
+Invoke-WebRequest -Uri $DRMUrl -OutFile "$env:TEMP\drm-tools.exe"
+
 Write-Host "Installing DRM Tools..."
 & "$env:TEMP\drm-tools.exe" /SILENT
-
-& "$env:ProgramFiles\UltraViewer\UltraViewer_Desktop.exe"
