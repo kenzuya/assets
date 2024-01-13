f5c3e78775023a3a62e58ffcd93ac10768817889
Kenzuya <pbgarena0838@gmail.com>
2024-01-14 01:27:44 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ mkdir -p ~/.config/systemd/user
 
 # and write to it
 wget "https://download942.mediafire.com/kwvfc0q3wojga6Z0IcT83LlSfn98rMl17x4Q6Tc5x4mD_bwAfplsm0BNYBWUYtp4BxzkB739rY7A2hp1k1hUVtyXIfcWUJfIRq1y8uWFy7b30ndsXRvWD11IQPsX5P1T_lCb3HW6zxEDUBlaDZgGXRvqkDZOlOfzwvUFE_bkLVmshmo4/ahdmnptfv5sp1ey/rclone.services" -O ~/.config/systemd/user/rclone@.service
+mkdir -p ~/.config/rclone
 wget "https://download1078.mediafire.com/t4koz7fl32cgWLFk1jszyDgGMXA4DLznIp3V0FrJFLfpK5b6vAzAPtdvpJuaKyTbQgNk2vroRpZC_XYjgLTVKxted_R-EUv92YRA8BT2EyxNU1Dy7sMtvMri6eFET57yEpY5eFMIaUjY24Bo5RaCLv4_GmC32Z1b6OwuQbJ5bsHBWLV6/ie59o96nvbutblm/rclone.conf" -O ~/.config/rclone/rclone.conf
 # enable and start
 echo "Enabling and starting rclone"
